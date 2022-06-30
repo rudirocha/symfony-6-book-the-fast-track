7cef7161058f99614e81a5d0d974a147c4eb3cef
Rúdi Rocha <rudi.rocha@gmail.com>
2022-06-30 22:07:48 +0000

    end of Admin crud and initial twig code

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,25 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-        /*return $this->render('conference/index.html.twig', [
+        return new Response($twig->render('conference/index.html.twig', [
+                        'conferences' => $conferenceRepository->findAll(),
+        ]));
+        return $this->render('conference/index.html.twig', [
             'controller_name' => 'ConferenceController',
-        ]);*/
-        return new Response(<<<EOF
-        <html>
-            <body>
-                <img src="/images/under-construction.gif" />
-            </body>
-        </html>
-        EOF
-        );
+        ]);
+    }
+
+    public function detail(Environment $twig, ConferenceRepository $conferenceRepository, CommentRepository $commentRepository)
+    {
+        
     }
 }
