d62e2d1f20a2a25966b171401607b0e41d203e6e
Rúdi Rocha <rudi.rocha@gmail.com>
2022-07-01 19:20:27 +0000

    twig

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,28 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
-    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
+
+    private $twig;
+
+    public function __construct(Environment $twig)
     {
-        return new Response($twig->render('conference/index.html.twig', [
+        $this->twig = $twig;
+    }
+
+    #[Route('/', name: 'homepage')]
+    public function index(ConferenceRepository $conferenceRepository): Response
+    {
+        return new Response($this->twig->render('conference/index.html.twig', [
                         'conferences' => $conferenceRepository->findAll(),
         ]));
         return $this->render('conference/index.html.twig', [
             'controller_name' => 'ConferenceController',
         ]);
     }
-
-    public function detail(Environment $twig, ConferenceRepository $conferenceRepository, CommentRepository $commentRepository)
+    #[Route('/conferences/{id}', name: 'conference')]
+    public function detail(Request $request, Conference $conference, CommentRepository $commentRepository)
     {
-        
+        $offset = max(0, $request->query->get('offset'));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+        return new Response(
+            $this->twig->render('conference/show.html.twig',[
+                'conference' => $conference,
+                'comments' => $paginator,
+                'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+                'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+         
+            ])
+        );
     }
 }
