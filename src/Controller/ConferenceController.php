172f289e880e77e6fafbe9b69b9d6a2f096837a2
Rúdi Rocha <rudi.rocha@gmail.com>
2022-07-31 21:13:00 +0000

    Preventing spam

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -6,6 +6,7 @@ use App\Entity\Comment;
 use App\Entity\Conference;
 use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
+use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -30,24 +31,24 @@ class ConferenceController extends AbstractController
     public function index(): Response
     {
         return new Response($this->twig->render('conference/index.html.twig', [
-                        //'conferences' => $conferenceRepository->findAll(),
+            //'conferences' => $conferenceRepository->findAll(),
         ]));
         return $this->render('conference/index.html.twig', [
             'controller_name' => 'ConferenceController',
         ]);
     }
     #[Route('/conferences/{slug}', name: 'conference')]
-    public function detail(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir)
+    public function detail(Request $request, Conference $conference, CommentRepository $commentRepository, SpamChecker $spamChecker, string $photoDir)
     {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
 
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $comment->setConference($conference);
 
             if ($photo = $form['photo']->getData()) {
-                $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
+                $filename = bin2hex(random_bytes(6)) . '.' . $photo->guessExtension();
                 try {
                     $photo->move($photoDir, $filename);
                 } catch (FileException $e) {
@@ -57,21 +58,32 @@ class ConferenceController extends AbstractController
             }
 
             $this->entityManager->persist($comment);
+            
+            $context = [
+                'user_ip' => $request->getClientIp(),
+                'user_agent' => $request->headers->get('user-agent'),
+                'referrer' => $request->headers->get('referer'),
+                'permalink' => $request->getUri(),
+            ];
+            if (2 === $spamChecker->getSpamScore($comment, $context)) {
+                throw new \RuntimeException('Blatant spam, go away!');
+            } 
+
             $this->entityManager->flush();
 
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
         }
- // TODO: upload files
+        // TODO: upload files
         $offset = max(0, $request->query->get('offset'));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
         return new Response(
-            $this->twig->render('conference/show.html.twig',[
+            $this->twig->render('conference/show.html.twig', [
                 'conference' => $conference,
                 'comments' => $paginator,
                 'comment_form' => $form->createView(),
                 'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
                 'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
-         
+
             ])
         );
     }
